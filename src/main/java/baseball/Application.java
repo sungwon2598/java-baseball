1224877aac933dba6000b1f6db196c999648abf7
sungwon2598 <tjddnjs2598@gmail.com>
2023-10-17 12:31:23 +0900

    asfkjiodskdsjkdddddddsssㅋㅁㄹㅁㅁㅇ

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,9 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("sfs");
+        System.out.println("sfs");
+        System.out.println("Saaaasss");
+        System.out.println("ww2tgwgs");
     }
 }
